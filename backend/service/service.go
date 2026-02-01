2888decfa6e4ddfabc78aa1ea33d29d19ce5696b
miguel <miguel.lopezp25@gmail.com>
2026-01-31 21:57:28 -0600

    modifi service to be more professional

--- backend/service/service.go
+++ backend/service/service.go
@@ -7,11 +7,13 @@ import (
 	"io"
 	"net/http"
 
+	"github.com/miguellopezp25-cell/crypto-tracker-go-react/backend/config"
 	schema "github.com/miguellopezp25-cell/crypto-tracker-go-react/backend/schemas"
 )
 
 func GetCryptoPriceBTC(symbol string) (schema.CryptoInfo, error) {
-	url := fmt.Sprintf("https://api.binance.com/api/v3/ticker/price?symbol=%s", symbol)
+	urlEnv := config.GetEnv("API_URL_CRYPTO")
+	url := fmt.Sprintf("%s%s", urlEnv, symbol)
 
 	response, err := http.Get(url)
 	if err != nil {
