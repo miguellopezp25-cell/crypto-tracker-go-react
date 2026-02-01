0664eec1ed0da3e05cc81c8480281a9c0370fb4f
miguel <miguel.lopezp25@gmail.com>
2026-01-31 19:15:25 -0600

    add get for see the cryptos

--- backend/api/server.go
+++ backend/api/server.go
@@ -2,7 +2,6 @@ package api
 
 import (
 	"fmt"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/miguellopezp25-cell/crypto-tracker-go-react/backend/config"
@@ -17,11 +16,9 @@ func StartServer() {
 
 	port := config.GetEnv("PORT")
 	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+
+	router.GET("/binance/tracking", getBinanceHandler)
+
 	router.Run(":" + port) // escucha en 0.0.0.0:8081 por defecto
 	fmt.Println("Server running on port " + port)
 }
--- backend/service/service.go
+++ backend/service/service.go
@@ -1,5 +1,42 @@
 package service
 
-func GetCryptodata() {
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+)
 
+type CryptoInfo struct {
+	Symbol string  `json:"symbol"`
+	Name   string  `json:"name"`
+	Price  float64 `json:"price"`
+}
+
+func GetCryptoPriceBTC(symbol string) (CryptoInfo, error) {
+	url := fmt.Sprintf("https://api.binance.com/api/v3/ticker/price?symbol=%s", symbol)
+
+	response, err := http.Get(url)
+	if err != nil {
+		return CryptoInfo{}, err
+	}
+	defer response.Body.Close()
+
+	// Validar si Binance respondió con error (ej: símbolo no encontrado)
+	if response.StatusCode != http.StatusOK {
+		return CryptoInfo{}, errors.New("símbolo no encontrado en Binance")
+	}
+
+	responseInfo, err := io.ReadAll(response.Body)
+	if err != nil {
+		return CryptoInfo{}, err
+	}
+
+	var cryptoInfo CryptoInfo
+	err = json.Unmarshal(responseInfo, &cryptoInfo)
+	if err != nil {
+		return CryptoInfo{}, err
+	}
+	return cryptoInfo, nil
 }
